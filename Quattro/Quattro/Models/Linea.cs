9433b9c0d50fe390433d2ecbe511c534da72cd9a
Andrés <ander74@gmail.com>
2018-06-29 20:07:03 +0200

    Actualización Rutinaria

---
+++ Quattro/Quattro/Models/Linea.cs
@@ -0,0 +1,19 @@
+﻿#region COPYRIGHT
+// ===============================================
+//   Quattro - Licencia GNU/GPL 3.0 - A.Herrero
+// -----------------------------------------------
+//  Vea el archivo Licencia.txt para más detalles 
+// ===============================================
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Text;
+using QuattroNet;
+
+namespace Quattro.Models {
+
+    public class Linea : NotifyBase {
+
+
+	}
+}
